c556776345920c6aeb3cfe3a5364b25b437764c4
s31431 <s31431@pjwstk.edu.pl>
2025-03-03 09:46:44 +0100

    Task2

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World3!");
+
+
+double GetAverage(int[] numbers)
+{
+    return 0;
+}
+
+int[] arr = { 1, 2, 3, 4, 5 };
+
+GetAverage(arr);
